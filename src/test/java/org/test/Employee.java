3039bc8bb3926d9e86fee6a88859dcbecea4486d
himachalamsri <sriramh115@gmail.com>
2024-01-11 07:41:46 +0530

    added

--- src/test/java/org/test/Employee.java
+++ src/test/java/org/test/Employee.java
@@ -1,5 +1,24 @@
 package org.test;
 
 public class Employee {
+	
+	private void test1() {
+		System.out.println("test1");
+
+	}
+	private void test2() {
+		System.out.println("test2");
+
+	}
+	private void test3() {
+		System.out.println("test3");
+	}
+	private void test4() {
+		System.out.println("test4");
+	}
+	private void test5() {
+		System.out.println("test5");
+
+	}
 
 }
